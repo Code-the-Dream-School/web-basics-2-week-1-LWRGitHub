d9dca1912d934f345f72d022beaf1fa2d830f405
LWRGitHub <loganreynoldslwr@gmail.com>
2020-06-15 17:25:02 -0700

    assignment DONE

--- index.js
+++ index.js
@@ -21,6 +21,9 @@ console.log('hello world')
 
 // TODO: ADD CODE HERE
 
+const myVar = "This is my var!";
+console.log(myVar);
+
 /* Challenge #2
  * Create variables for your first and last name then print your full name
  *
@@ -33,6 +36,10 @@ console.log('hello world')
  */
 
 // TODO: ADD CODE HERE
+
+const firstName = 'Logan';
+const lastName = 'Reyolds';
+console.log(`${firstName} ${lastName}`);
 
 /* Challenge #3
  * Calculate the total price rounded to two decimals
@@ -47,6 +54,11 @@ console.log('hello world')
  */
 
 // TODO: ADD CODE HERE
+
+const price = 9.98;
+const quantity = 5;
+const total = price * quantity;
+console.log(`$${total.toFixed(2)}`)
 
 /* Challenge #4
  * Declare a variable and conditionally assign it a value
@@ -63,6 +75,16 @@ console.log('hello world')
  */
 
 // TODO: ADD CODE HERE
+const weather = 'Rainy';
+let message;
+
+if(weather === 'Sunny'){
+    message = "Yay! It's sunny outside";
+} else {
+    message = "Aww! It's not sunny outside";
+}
+
+console.log(message);
 
 /* Challenge #5
  * Create a function that returns any string
@@ -77,6 +99,11 @@ console.log('hello world')
 
 // TODO: ADD CODE HERE
 
+function myFunction(){
+    return "A random string!";
+}
+console.log(myFunction());
+
 /* Challenge #6
  * Rewrite the function in Challenge #5 as an arrow function
  *
@@ -89,6 +116,11 @@ console.log('hello world')
  */
 
 // TODO: ADD CODE HERE
+
+const myArrowFunction = () =>{
+    return "Random string!";
+}
+console.log(myArrowFunction());
 
 /* Challenge #7
  * Create a function that accepts a string argument and returns it in all uppercase
@@ -103,6 +135,12 @@ console.log('hello world')
  */
 
 // TODO: ADD CODE HERE
+
+const stringToUpper = str =>{
+    return str.toUpperCase();
+}
+const myStr = "All letters should be big!";
+console.log(stringToUpper(myStr));
 
 /* Challenge #8
  * Create a function that accepts a number argument and returns double its value
@@ -120,3 +158,11 @@ console.log('hello world')
  */
 
 // TODO: ADD CODE HERE
+
+const multiplyByTwo = number =>{
+    return number * 2;
+}
+const num1 = multiplyByTwo(1);
+const num2 = multiplyByTwo(5);
+const num3 = multiplyByTwo(10);
+console.log(num1, num2, num3);
